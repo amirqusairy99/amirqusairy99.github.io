1356cbf5f6a79c5c1f4e4282ddd141f44146a067
Mohammad Amir Qusairy <amirqusairy7@gmail.com>
2026-03-10 06:42:27 +0000

    Initial commit: Jekyll site

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1773120643',resources: [
+    cacheName: 'chirpy-1773124715',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
